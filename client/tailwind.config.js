eaa5e44c969ca8bd6e135594080d22c16b965e0a
Bobby <bobur0218programmer@gmail.com>
2024-10-31 20:08:40 +0500

    :tada: initialized - new react ts tailwindcss app

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "720px",
+          xl: "720px",
+          "2xl": "900px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
